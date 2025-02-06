9d5ec993a09501e0a71a9d32ef48b09eee1a4db7
Hyukdroians <hyukdroians@naver.com>
2025-02-06 12:26:55 +0900

    edit Base

--- archives/script.js
+++ archives/script.js
@@ -1,21 +1,16 @@
 // "Hyukdroians" 글자 애니메이션
-const letters = document.querySelectorAll(".rotate span");
+        const letters = document.querySelectorAll(".rotate span");
 
-function animateLetters() {
-    letters.forEach((letter, index) => {
-        setTimeout(() => {
-            letter.style.fontWeight = "900"; // 굵게 변경
-            setTimeout(() => {
-                letter.style.fontWeight = "100"; // 다시 원래대로
-            }, 1300); // 0.3초 후 원래 굵기로
-        }, index * 150); // 각 글자의 애니메이션을 0.15초 간격으로 실행
-    });
-}
+        function animateLetters() {
+            letters.forEach((letter, index) => {
+                setTimeout(() => {
+                    letter.style.fontWeight = "900"; // 굵게 변경
+                    setTimeout(() => {
+                        letter.style.fontWeight = "100"; // 다시 원래대로
+                    }, 1300); // 0.3초 후 원래 굵기로
+                }, index * 150); // 각 글자의 애니메이션을 0.15초 간격으로 실행
+            });
+        }
 
-// 일정 간격으로 반복 실행
-setInterval(animateLetters, 2000); // 2초마다 반복 실행
-
-// 페이지 로딩이 완료되면 로딩 화면 숨기기
-window.onload = function() {
-    document.getElementById('loadingScreen').style.display = 'none';
-};
+        // 일정 간격으로 반복 실행
+        setInterval(animateLetters, 2000); // 2초마다 반복 실행
