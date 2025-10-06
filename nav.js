1f89ad760a636851a858a9a88c16923da08168f9
Hyukdroians <hyukdroians@naver.com>
2025-10-06 19:45:06 +0900

    JS 코드 수정

--- nav.js
+++ nav.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".frame .div");
-  const currentPath = window.location.pathname;
+  const currentPath = window.location.pathname.replace(/\/$/, '');
 
   links.forEach(link => {
-    if (link.getAttribute("href") === currentPath) {
+    const href = link.getAttribute("href").replace(/\/$/, ''); 
+    if (currentPath === href || currentPath.startsWith(href + "/")) {
       link.classList.add("active");
     }
   });
